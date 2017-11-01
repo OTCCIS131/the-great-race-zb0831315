e9fbf448749807daefe3fb9c6214ebf2e17f6096
zb0831315 <zb0831315@otc.edu>
2017-10-31 21:15:37 -0500

    Almost Finished(need to get racers to stop correctly)

--- great-race.js
+++ great-race.js
@@ -9,6 +9,73 @@ $(function () {
             tick: 0,
             interval: null
         },
-    })
+        computed: {
+            winning() {
+                if (this.volibear == this.rammus) return null
 
+                return this.volibear > this.rammus ? 'Volibear' : 'Rammus'
+            },
+            rammusStyle() {
+                return {
+                    left: `${this.rammus}vw`
+                }
+            },
+            rammusClass() {
+                if (!this.winner) return
+                return this.winner == 'Rammus'
+            },
+            voliStyle() {
+                return {
+                    left: `${this.volibear}vw`
+                }
+            },
+            voliClass() {
+                if (!this.winner) return
+                return this.winner == 'Volibear'
+            }
+        },
+        methods: {
+            startRace() {
+                if (this.winner) {
+                    this.restart()
+                    return
+                }
+            this.racing = true
+
+            this.interval = setInterval(() => {
+                this.progressPlayers()
+            }, 50)
+        },
+        progressPlayers() {
+            this.tick++
+            this.volibear += (Math.random() >= Math.random()) ? 1 : 0
+            this.rammus += (Math.random() >= Math.random()) ? 1 : 0
+            this.checkVicktory()
+        },
+        checkVictory() {
+            if (this.volibear == this.rammus) return
+
+            if (this.volibear >= 90) {
+                this.declareVictory('Volibear')
+            }
+
+            if (this.rammus >= 90) {
+                this.declareVictory('Rammus')
+            }
+        },
+        declareVictory(player) {
+            clearInterval(this.interval)
+            this.interval = null
+            this.racing = false
+            this.winner = player
+        },
+        restart() {
+            this.racing = false
+            this.winner = null 
+            this.volibear = 0
+            this.rammus = 0
+            this.tick = 0
+        }
+    }
 })
+});
