92ab5caf486bb1201bea4ee0633a3457637a2da9
zb0831315 <zb0831315@otc.edu>
2017-10-31 20:20:09 -0500

    fixed placement, started adding js

---
+++ great-race.js
@@ -0,0 +1,14 @@
+$(function () {
+    let vm = new Vue({
+        el: '#app',
+        data: {
+            racing: false,
+            winner: null,
+            volibear: 0,
+            rammus: 0,
+            tick: 0,
+            interval: null
+        },
+    })
+
+})
